9a579ebd3176338ccf8ce5119ca8a3be6f57dda6
Dylan Cook <dcook@email.neit.edu>
2024-03-11 11:21:12 -0400

    Layout and layout colors adjusted, updated bootstrap to v5.3, navbar and main layout complete.

---
+++ Pages/Experience.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RubinPortfolio.Pages
+{
+    public class ExperienceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
