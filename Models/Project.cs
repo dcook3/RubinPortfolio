6d99a3d0a1f3e6023482ab519bc183a18db0f4d3
Dylan Cook <dcook@email.neit.edu>
2024-03-13 15:09:26 -0400

    Added media files temporarily and started work on index caraousel and front-end data handling.

---
+++ Models/Project.cs
@@ -0,0 +1,33 @@
+﻿namespace RubinPortfolio.Models
+{
+    public class Project
+    {
+        private string name { get; set; }
+        private string shortDescription { get; set; }
+        private string description { get; set; }
+        private string duration { get; set; }
+        private string role { get; set; }
+        private string projectUrl { get; set; }
+        private string[] responsibilities { get; set; } = new string[0];
+        private string[] imageNames { get; set; } = new string[0];
+        private string[] imageUrls { get; set; } = new string[0];
+
+        public Project(string name, string shortDescription, string description, string duration, string role, string projectUrl, string[] responsibilities, string[] imageNames, string[] imageUrls)
+        {
+            this.name = name;
+            this.shortDescription = shortDescription;
+            this.description = description;
+            this.duration = duration;
+            this.role = role;
+            this.projectUrl = projectUrl;
+            this.responsibilities = responsibilities;
+            this.imageNames = imageNames;
+            this.imageUrls = imageUrls;
+        }
+
+        public static string GetJsonAsString(string filePath)
+        {
+            return File.ReadAllText(filePath);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.Web.BrowserLink;
+using RubinPortfolio.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+var mvcBuilder = builder.Services.AddRazorPages();
+
+// Enable runtime compilation when in development environment
+if (builder.Environment.IsDevelopment())
+{
+    mvcBuilder.AddRazorRuntimeCompilation();
+}
 
 var app = builder.Build();
 
@@ -11,6 +21,10 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
