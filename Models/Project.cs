ea5f3910caa2e6ea4051312aceb8c9df03f5c9c3
Dylan Cook <dcook@email.neit.edu>
2024-03-13 21:57:32 -0400

    Added razor code to make carousel, and dummy data for temporary project data perminence.

--- Models/Project.cs
+++ Models/Project.cs
@@ -2,32 +2,36 @@
 {
     public class Project
     {
-        private string name { get; set; }
-        private string shortDescription { get; set; }
-        private string description { get; set; }
-        private string duration { get; set; }
-        private string role { get; set; }
-        private string projectUrl { get; set; }
-        private string[] responsibilities { get; set; } = new string[0];
-        private string[] imageNames { get; set; } = new string[0];
-        private string[] imageUrls { get; set; } = new string[0];
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string Description { get; set; }
+        public string Duration { get; set; }
+        public string Role { get; set; }
+        public string ProjectUrl { get; set; }
+        public string[] Responsibilities { get; set; } = new string[0];
+        public string[] ImageNames { get; set; } = new string[0];
+        public string[] ImageUrls { get; set; } = new string[0];
 
         public Project(string name, string shortDescription, string description, string duration, string role, string projectUrl, string[] responsibilities, string[] imageNames, string[] imageUrls)
         {
-            this.name = name;
-            this.shortDescription = shortDescription;
-            this.description = description;
-            this.duration = duration;
-            this.role = role;
-            this.projectUrl = projectUrl;
-            this.responsibilities = responsibilities;
-            this.imageNames = imageNames;
-            this.imageUrls = imageUrls;
+            this.Name = name;
+            this.ShortDescription = shortDescription;
+            this.Description = description;
+            this.Duration = duration;
+            this.Role = role;
+            this.ProjectUrl = projectUrl;
+            this.Responsibilities = responsibilities;
+            this.ImageNames = imageNames;
+            this.ImageUrls = imageUrls;
         }
 
         public static string GetJsonAsString(string filePath)
         {
             return File.ReadAllText(filePath);
+        }
+        public override string ToString()
+        {
+            return $"Name:{this.Name}, \nShort Desc: {this.ShortDescription},\nDesc: {this.Description},\nDuration: {this.Duration}\nRole: {this.Role},\nProject Url: {this.ProjectUrl},\nResponsibilities@0: {this.Responsibilities[0]},\nImage Names@0: {this.ImageNames[0]},\nImage Urls@0: {this.ImageNames[0]}";
         }
     }
 }
